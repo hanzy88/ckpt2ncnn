ea1991a208dad7a83b5d89433476428d3526926b
hanzy88 <36069610+hanzy88@users.noreply.github.com>
2019-09-02 13:43:01 +0800

    Delete getNode.py

--- getNode.py
+++
@@ -1,6 +0,0 @@
-from tensorflow.python import pywrap_tensorflow
-checkpoint_path = 'models/expModel.ckpt'
-reader = pywrap_tensorflow.NewCheckpointReader(checkpoint_path)
-var_to_shape_map = reader.get_variable_to_shape_map()
-for key in var_to_shape_map:
-    print("tensor_name: ", key)
