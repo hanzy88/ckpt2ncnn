ebabd4b557e79646d09fd65896d861ba71d26683
hanzy88 <36069610+hanzy88@users.noreply.github.com>
2019-09-02 13:42:43 +0800

    Delete convert2pb.py

--- convert2pb.py
+++
@@ -1,5 +0,0 @@
-from tensorflow.python.tools import freeze_graph
-
-freeze_graph.freeze_graph('./models/exp.pbtxt', "", False,
-                        "./models/expModel.ckpt", "comExp/ouput/Conv2D",
-                        "save/restore_all", "save/Const:0", "./models/exp.pb", True, "")
